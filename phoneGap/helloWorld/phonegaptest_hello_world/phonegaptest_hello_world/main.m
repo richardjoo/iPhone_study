d3d0e2ca3118caace2d16c6560566fb6992c6684
Richard Joo <richardjoo@gmail.com>
2011-09-24 02:41:29 -0700

    created very first test page - hello world

---
+++ phoneGap/helloWorld/phonegaptest_hello_world/phonegaptest_hello_world/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  phonegaptest_hello_world
+//
+//  Created by Richard Joo on 9/24/11.
+//  Copyright ___ORGANIZATIONNAME___ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
